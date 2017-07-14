6c6f3471693d3cfa7d7adf729e998d58cdb1d060
sahuri <huriezu21@gmail.com>
2017-07-14 19:17:37 +0700

    Develop dashboard UI

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -14,5 +14,11 @@ function dashboardController($rootScope,$scope, $state) {
 	
 	var vm = this;
 	vm.title="JerseyRESTfull-NodeJs-AngularJs";
+	
+	vm.property={
+			lamps:{unit:10},
+			doors:{unit:5},
+			gates:{unit:2}
+	};
 }
 })();
